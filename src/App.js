05e0baeff1e749d96df7094e73d783232e80f93a
jack <jckhayes1@gmail.com>
2019-05-22 21:09:08 +1000

    Added the spinner component to the app.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import LoadingSpinner from './assets/components/loading_spinner/LoadingSpinner.connector';
+
 const App = () => {
   return (
     <div className="app">
+      <LoadingSpinner />
     </div>
   );
 }
