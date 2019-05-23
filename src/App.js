e45d926994343bf3beaadc8393c4f7c16fe80cf4
jack <jckhayes1@gmail.com>
2019-05-23 16:45:31 +1000

    Added the layout css module.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import layout from './assets/styles/layout.module.css';
 import LoadingSpinner from './assets/components/loading_spinner/LoadingSpinner.connector';
 
 const App = () => {
   return (
-    <div className="app">
+    <div class={layout.appContainer}>
       <LoadingSpinner />
     </div>
   );
