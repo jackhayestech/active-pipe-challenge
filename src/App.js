0c8f59f484a7078857053b7d7b7552a2b9bf9fe9
jackhayes <contact@jackhayes.tech>
2019-05-24 15:15:27 +1000

    Added the properties connector.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { appContainer } from './assets/styles/layout.module.css';
 
 import StartupSpinnerWrapper from './assets/components/layout/startup_spinner_wrapper/StartupSpinnerWrapper.connector';
+import PropertiesWrapper from './assets/components/properties/PropertiesWrapper.connector';
 
 const App = () => (
   <div className={appContainer}>
     <StartupSpinnerWrapper />
+    <PropertiesWrapper />
   </div>
 );
 
---
+++ src/assets/components/properties/PropertiesWrapper.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { propertiesWrapper } from '../../styles/properties.module.css';
+
+const PropertiesWrapper = () => (
+  <div className={propertiesWrapper}>
+    
+  </div>
+);
+
+export default PropertiesWrapper;
