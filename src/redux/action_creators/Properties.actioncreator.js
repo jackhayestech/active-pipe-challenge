aba3130ec0df8463195df94adcbadcc12060e57d
jackhayes <contact@jackhayes.tech>
2019-05-24 15:02:37 +1000

    Toggle loading display on data load.

--- src/redux/action_creators/Properties.actioncreator.js
+++ src/redux/action_creators/Properties.actioncreator.js
@@ -1,9 +1,15 @@
 import PropertiesActions from '../actions/Properties.actions';
 import { LOAD_PROPERTIES } from '../reducers/Properties.reducer'; 
+import { TOGGLE_LOADING_DISPLAY } from '../reducers/Ui.reducer'
 
 // Loads property data
 export const loadProperties = () => async (dispatch) => {
   const res = await PropertiesActions.load();
+
+  dispatch({
+    type: TOGGLE_LOADING_DISPLAY,
+    data: false,
+  })
 
   return (
     dispatch({
--- src/redux/reducers/Ui.reducer.js
+++ src/redux/reducers/Ui.reducer.js
@@ -1,3 +1,5 @@
+export const TOGGLE_LOADING_DISPLAY = "TOGGLE_LOADING_DISPLAY";
+
 const initialState = {
   errorDisplay: false,
   loadingDisplay: true,
@@ -5,6 +7,11 @@ const initialState = {
 
 const Ui = (state = initialState, action) => {
   switch (action.type) {
+    case TOGGLE_LOADING_DISPLAY:
+      return {
+        ...state,
+        loadingDisplay: action.data,
+      };
     default:
       return state;
   }
