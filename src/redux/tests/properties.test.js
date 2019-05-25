3af6e3f38d787532f338b5fe04cc6091da1163b5
jack <jckhayes1@gmail.com>
2019-05-25 16:55:07 +1000

    Properties unit tests.

---
+++ src/redux/tests/properties.test.js
@@ -0,0 +1,44 @@
+import store from '../../store';
+
+import { mockFetch } from './helpers/mock.helpers';
+import { failedLoading, sucessfulLoad } from './data/properties.data'
+import { loadProperties } from '../action_creators/Properties.actioncreator';
+
+/**
+ * Test loading of failed properties
+ *
+ * Expect Loading display to be false
+ * Expect alert display to be true
+ * Expect alert type to be error
+ */
+test('should fail loading property data', async () => {
+  mockFetch(failedLoading);
+  await store.dispatch(loadProperties());
+
+  expect(store.getState().Ui.loadingDisplay).toEqual(false);
+  expect(store.getState().Ui.alert.display).toEqual(true);
+  expect(store.getState().Ui.alert.type).toEqual('error');
+
+  //Reset state after test
+  store.dispatch({ type: 'RESET' });
+});
+
+/**
+ * Test loading of properties
+ *
+ * Expect Loading display to be false
+ * Expect alert display to be true
+ * Expect alert type to be error
+ */
+test('should load property data', async () => {
+  mockFetch(sucessfulLoad);
+  await store.dispatch(loadProperties());
+
+  expect(store.getState().Ui.loadingDisplay).toEqual(false);
+  expect(store.getState().Ui.alert.display).toEqual(false);
+  expect(store.getState().Properties.data).toEqual(sucessfulLoad);
+
+  //Reset state after test
+  store.dispatch({ type: 'RESET' });
+});
+
